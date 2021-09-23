47462458eafa24aec67294dd6973a941a52c90b4
yuna__ <yuna744@gmail.com>
2021-09-23 18:58:31 +0900

    promise, async, await

--- promise/example6.js
+++ promise/example6.js
@@ -2,6 +2,10 @@
 executor 함수 인자 중 하나인 reject 함수를 실행하면, rejected(거부) 상태가 됩니다.
 */
 
-new Promise((resolve,reject) => {
+new Promise((resolve,reject) => {//대기, 거부 
     reject(); // rejected 상태
 });
+
+new Promise((resolve,reject)=>{
+    reject();
+})
